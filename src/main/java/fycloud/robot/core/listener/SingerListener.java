ae6d53b5a3dcd4ff6fd9a8f779e0a53a46cc1c70
Varley.Erez <1963460510@qq.com>
2022-05-09 20:22:10 +0800

    新增 娱乐功能（唱歌）

---
+++ src/main/java/fycloud/robot/core/listener/SingerListener.java
@@ -0,0 +1,39 @@
+package fycloud.robot.core.listener;
+
+import catcode.CatCodeUtil;
+import catcode.CodeBuilder;
+import catcode.Neko;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import fycloud.robot.core.APIs;
+import fycloud.robot.util.HttpUtil;
+import lombok.extern.slf4j.Slf4j;
+import love.forte.common.ioc.annotation.Beans;
+import love.forte.simbot.annotation.Filter;
+import love.forte.simbot.annotation.OnGroup;
+import love.forte.simbot.api.message.events.GroupMsg;
+import love.forte.simbot.api.sender.Sender;
+import love.forte.simbot.filter.MatchType;
+
+/**
+ * @author 19634
+ * @version 1.0
+ * @date 2022/5/9 20:11
+ */
+@Beans
+@Slf4j
+public class SingerListener {
+    @OnGroup
+    @Filter(value = "(唱首歌|来首歌|唱歌)", matchType = MatchType.REGEX_MATCHES)
+    public void listen(GroupMsg msg, Sender sender) {
+        log.info(msg.getAccountInfo().getAccountNickname() + "(" + msg.getAccountInfo().getAccountCode() + ") 在 " + msg.getGroupInfo().getGroupName() + "(" + msg.getGroupInfo().getGroupCode() + ") "+" 调用了 <唱歌> 功能--> " + msg.getText());
+        JSONObject response = JSON.parseObject(HttpUtil.doGET(APIs.XiaoBai_API.Sing));
+        JSONObject data = response.getJSONObject("data");
+        String audioUrl = data.getString("audioSrc");
+        final CodeBuilder<String> codeBuilder = CatCodeUtil.getInstance().getStringCodeBuilder("record", false);
+        String catCode = codeBuilder
+                .key("file").value(audioUrl)
+                .build();
+        sender.sendGroupMsg(msg, catCode);
+    }
+}
