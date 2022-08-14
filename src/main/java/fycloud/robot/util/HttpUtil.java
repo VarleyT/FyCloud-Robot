8cf3d7501773e205aa24cf8df6d0d6e0070ae8c1
Varley.Erez <1963460510@qq.com>
2022-08-14 20:01:24 +0800

    更新 fastjson版本

--- src/main/java/fycloud/robot/util/HttpUtil.java
+++ src/main/java/fycloud/robot/util/HttpUtil.java
@@ -2,7 +2,6 @@ package fycloud.robot.util;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import fycloud.robot.FyRobotApp;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 
